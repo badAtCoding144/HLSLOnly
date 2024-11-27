12db8f46ec73e51acaa8dcb3f29976e3a70968e2
badAtCoding144 <88886499+badAtCoding144@users.noreply.github.com>
2024-11-27 23:44:43 +0200

    got

---
+++ HLSL/AspectRatio.glsl
@@ -0,0 +1,14 @@
+uniform vec2 iResolution;
+
+void main() {
+  vec2 uv = gl_FragCoord.xy / iResolution.xy;
+  float ratio = iResolution.x/iResolution.y;
+  float Len = length(vec2((uv.x-0.5)*ratio,uv.y-0.5));
+
+  
+  float Dist = distance(Len,0.125);
+  float IsCloseEnough = step(Dist,0.125);
+  
+  
+  gl_FragColor = vec4(IsCloseEnough, 0.0, 0.0, 1.0);
+}
